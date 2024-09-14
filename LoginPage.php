dd16dad468f458af859d9d110f6946bed71e32ea
DanielStrath <daniel.morara@strathmore.edu>
2024-09-14 17:52:00 +0300

    more css styling of login page

--- LoginPage.php
+++ LoginPage.php
@@ -9,17 +9,33 @@ include "layouts/header.php";
 <div class="login-container" >
     <h1>Login</h1>
     <form action=""> 
+
         <div class="input-container">
             <label for="username">Username:</label>
             <br>
-            <input type="text" id="username" name="username">
+            <input type="text" id="username" name="username" required>
         </div>
+
         <div class="input-container">
             <label for="password">Password:</label>
             <br>
-            <input type="password" id="password" name="password" placeholder="********">
+            <input type="password" id="password" name="password" placeholder="********" required>
         </div>
+
+        <div class="preferences">
+            <div class="remember-container">
+                <input type="checkbox" id="remember">
+                <label for="remember">Remember me?</label>
+            </div>
+
+            <div class="password">
+                <a href="">Forgot Password?</a>
+            </div>
+        </div>
+
         <button type="submit">Login</button>
+
+        <p>Don't have an account? <a href="SignupPage.php">Signup</a></p>
     </form>
 </div>
 
