d281377f2aba58d925631ce4aee55e865aa18d8e
Rehana-Kerubo <166706039+Rehana-Kerubo@users.noreply.github.com>
2024-09-17 01:27:27 +0300

    Rehana tried putting some css to the homepage

--- HomePage.php
+++ HomePage.php
@@ -2,9 +2,9 @@
 $pagetitle = "Home Page";
 // Include the header
 include('layouts/header.php');
-?>
+?> 
 <head>
-    <link rel="stylesheet" href="HomePage.css">
+    <link rel="stylesheet" href="style.css">
     <!-- <link rel="stylesheet" href="adminStyle.css"> -->
 </head>
 
@@ -31,7 +31,20 @@ include('layouts/header.php');
                 <p>Explore the globe's captivating destinations with our cerated travel packages.</p>
 
                 <div class="carousel">
-                    <!-- elements for the carousel -->
+                   
+                    <div class="carousel-img">
+                     <div class="left">
+                        <img src="img/carousel1.jpeg" alt="Travel Package 1">
+                     </div>
+                     <div class="main">
+                        <img src="img/carousel2.jpeg" alt="Travel Package 2">
+                     </div>
+                     <div class="right">
+                        <img src="img/carousel3.jpeg" alt="Travel Package 3">
+                     </div>
+                    </div>
+                   
+
                 </div>
 
             </div>
@@ -41,7 +54,22 @@ include('layouts/header.php');
                 <p>From serene mountainsgetaways to vibrant culltural hubs, we'll help you craft the ultimate journey tailours to your passions</p>
 
                 <div class="img-type">
-                    <!-- images for the packages -->
+                    <div class="holiday-card">
+                        <img src="img/mountainsType.jpeg" alt="Holiday 1">
+                        <p>Mountains</p>
+                    </div>
+                    <div class="holiday-card">
+                        <img src="img/beachType.jpeg" alt="Holiday 2">
+                        <p>Beaches</p>
+                    </div>
+                    <div class="holiday-card">
+                        <img src="img/type3.jpeg" alt="Holiday 3">
+                        <p>Culture</p>
+                    </div>
+                    <div class="holiday-card">
+                        <img src="img/wildlifeType.jpeg" alt="Holiday 4">
+                        <p>Wildlife</p>
+                    </div>
                 </div>
             </div>
 
--- layouts/header.php
+++ layouts/header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pagetitle ?></title>
     <link rel = "icon" href="img/logo2.png" type = "image/png">
+    <link rel= "stylesheet" href = "style.css">
+    
 </head>
 <body>
     <div class="nav">
