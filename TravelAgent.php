a4fcfabe3ad72c7c6859f8c96660c3e4285b6099
Ethan Bwibo <enbwibo@gmail.com>
2024-10-01 21:51:38 +0300

    Travel Agent Dashboard

--- TravelAgent.php
+++ TravelAgent.php
@@ -7,28 +7,33 @@ include "layouts/header.php";
 
     <div class="main">
         <div class="sidebar">
-            <h2>DASHBOARD</h2>
+            <h2>PANEL</h2>
             <ul>
 
                 <div class="sidebar-link">
-                    <li><a href="">User Management </a></li>
+                    <li><a href="">Packages </a></li>
                     <img src="img/user.png" alt="User Icon">
                 </div>
 
                 <div class="sidebar-link">
-                    <li><a href="">Travel Packages </a></li>
+                    <li><a href="">Booking Management </a></li>
                     <img src="img/travelpackage.png" alt="Package Icon">
                 </div>
                     
                 <div class="sidebar-link">
-                    <li><a href="">Booking Management </a></li>   
+                    <li><a href="">Customer Support </a></li>   
                     <img src="img/book.png" alt="Book Icon">
                 </div>
 
                 <div class="sidebar-link">
                     <li><a href="">Reports & Analytics </a></li>
                     <img src="img/report.png" alt="Report Icon">
-                </div>           
+                </div>    
+                
+                <div class="sidebar-link">
+                    <li><a href="">Reviews and Feedback </a></li>
+                    <img src="img/report.png" alt="Report Icon">
+                </div> 
             </ul>
         </div>
 
