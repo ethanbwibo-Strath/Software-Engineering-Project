5831cc3aa88244cae28913d97a21b50630c60486
DanielStrath <daniel.morara@strathmore.edu>
2024-10-06 09:30:19 +0300

    created a sidebar for the userdashboard

---
+++ sidebar.php
@@ -0,0 +1,22 @@
+<?php
+$pagetitle = "User Dashboard";
+include 'layouts/header.php';
+?>
+
+<link rel="stylesheet" href="sidebar.css">
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+<link rel="json" href="sidebar.js">
+
+
+<div id="userDashboardContent">
+    <div class="sidebar">
+        <header>Dashboard</header>
+        <ul>
+            <li><a href="#" class="nav-item"><i class='bx bxs-user-account'></i>Account Details</a></li>
+            <li><a href="#" class="nav-item"><i class='bx bxs-plane-alt'></i>My Trips</a></li>
+            <li><a href="#" class="nav-item"><i class='bx bx-help-circle'></i>Help</a></li>
+            <li><a href="#" class="nav-item"><i class='bx bx-calendar-check'></i>Bookings</a></li>
+            <li><a href="#" class="nav-item"><i class='bx bxs-user-x'></i>Delete Account</a></li>
+        </ul> 
+    </div>
+</div>
