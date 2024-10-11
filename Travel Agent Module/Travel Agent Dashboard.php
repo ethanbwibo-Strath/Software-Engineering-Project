43bdb648ecc93ca84468ae16a5e85988311daef1
Ethan Bwibo <enbwibo@gmail.com>
2024-10-11 14:49:20 +0300

    Removing Old TravelAgent Files

--- Travel Agent Module/Travel Agent Dashboard.php
+++ Travel Agent Module/Travel Agent Dashboard.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width = device-width, initial-scale = 1.0">
 
-    <title> Trial Travel Agent Dash</title>
+    <title>Travel Agent Dashboard</title>
     <link rel = "icon" href="../img/logo2.png" type = "image/png">
-    <link rel="stylesheet" href= "trial.css">
+    <link rel="stylesheet" href= "Travel Agent Dashboard.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
 </head>
