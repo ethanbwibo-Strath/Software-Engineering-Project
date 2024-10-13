2289926a94f5211de0ce174d1b4e17df95ff072b
Ethan Bwibo <enbwibo@gmail.com>
2024-10-13 19:56:27 +0300

    ...

--- NewHomePage.php
+++ NewHomePage.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width = device-width, initial-scale = 1.0">
 
-    <title> NewHomePage</title>
+    <title> HomePage </title>
     <link rel = "icon" href="../img/logo2.png" type = "image/png">
     <link rel="stylesheet" href= "NewHomePage.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
