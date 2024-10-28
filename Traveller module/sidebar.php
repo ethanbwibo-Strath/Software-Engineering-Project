64a66be3b861b93479c3d890a474a61429d3ab2c
VictorNzai <victor.nzai@strathmore.edu>
2024-10-28 08:47:31 +0300

    directory changes

--- Traveller module/sidebar.php
+++ Traveller module/sidebar.php
@@ -4,7 +4,7 @@
             <li><a href="accountdetails.php" class="nav-item"><i class='bx bxs-user-account'></i>Account Details</a></li>
             <li><a href="#" class="nav-item"><i class='bx bxs-plane-alt'></i>My Trips</a></li>
             <li><a href="help.php" class="nav-item"><i class='bx bx-help-circle'></i>Help</a></li>
-            <li><a href="..Travel Agent Module/AllAboutPackages/viewPackages.php" class="nav-item"><i class='bx bx-calendar-check'></i>Bookings</a></li>
+            <li><a href="../Travel Agent Module/AllAboutPackages/viewPackages.php" class="nav-item"><i class='bx bx-calendar-check'></i>Bookings</a></li>
             <li><a href="#" class="nav-item"><i class='bx bxs-user-x'></i>Delete Account</a></li>
             <li><a href="logout.php" class="nav-item"><i class='bx bx-log-out'></i>Logout</a></li>
         </ul> 
