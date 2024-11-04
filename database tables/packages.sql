7c74b7af30339d27b45cbee40c1fc355a8f78ce2
VictorNzai <victor.nzai@strathmore.edu>
2024-11-04 14:03:28 +0300

    Test

--- database tables/packages.sql
+++ database tables/packages.sql
@@ -10,3 +10,6 @@ CREATE TABLE `packages` (
   `package_image` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
