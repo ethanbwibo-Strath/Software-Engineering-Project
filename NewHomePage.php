8c76db57ef0e3516b260f9f2397f3d06aa86e86c
DanielStrath <daniel.morara@strathmore.edu>
2024-11-05 11:41:05 +0300

    booking mods

--- NewHomePage.php
+++ NewHomePage.php
@@ -44,7 +44,7 @@ session_start();
                 <li class="dropdown">
                     <a href="#" class="dropbtn"><?php echo htmlspecialchars($_SESSION['username']); ?> <i class='bx bx-chevron-down'></i></a>
                     <div class="dropdown-content">
-                        <a href="accountDetails.php">Account Details</a>
+                        <a href="Traveller Module/accountdetails.php">Account Details</a>
 
                         <?php if ($_SESSION['account_type'] == 'admin') : ?>
                             <a href="Admin Module/adminDashboard.php">Admin Dashboard</a>
@@ -53,7 +53,7 @@ session_start();
                             <a href="Travel Agent Module/Travel Agent Dashboard.php">Agent Dashboard</a>
 
                         <?php elseif ($_SESSION['account_type'] == 'traveler') : ?>
-                            <a href="travelerDashboard.php">My Bookings</a>
+                            <a href="Traveller Module/booking/bookings.php">My Bookings</a>
                         <?php endif; ?>
 
                         <a href="Traveller Module/logout.php">Logout</a>
