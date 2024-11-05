efc6ad481c3bf539fa622177f1f0b85dc5954ccc
Rehana-Kerubo <166706039+Rehana-Kerubo@users.noreply.github.com>
2024-11-05 19:29:29 +0300

    Rehana created a pagee for the traveller to view their booked trips. It does not work fully because i do not know what data should go to the database.

--- Traveller module/sidebar.php
+++ Traveller module/sidebar.php
@@ -2,7 +2,7 @@
         <header>PANEL</header>
         <ul>
             <li><a href="accountdetails.php" class="nav-item"><i class='bx bxs-user-account'></i>Account Details</a></li>
-            <li><a href="#" class="nav-item"><i class='bx bxs-plane-alt'></i>My Trips</a></li>
+            <li><a href="myTrips.php" class="nav-item"><i class='bx bxs-plane-alt'></i>My Trips</a></li>
             <li><a href="help.php" class="nav-item"><i class='bx bx-help-circle'></i>Help</a></li>
             <li><a href="booking/bookings.php" class="nav-item"><i class='bx bx-calendar-check'></i>Bookings</a></li>
             <li><a href="#" class="nav-item"><i class='bx bxs-user-x'></i>Delete Account</a></li>
