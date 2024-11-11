f08e22438b54eba7556725bb564ad8304760b882
Ethan Bwibo <enbwibo@gmail.com>
2024-11-11 19:45:05 +0300

    icon color changed

--- Traveller module/dbconnection.php
+++ Traveller module/dbconnection.php
@@ -3,11 +3,7 @@ class dbConnection {
     private $host = 'localhost';
     private $dbname = 'cheapthrills';
     private $username = 'root';
-<<<<<<< HEAD
-    private $password = 'mySQLpass_11!';
-=======
-    private $password = 'rehanais2cool';
->>>>>>> 8979cb9e80ef13a9555a1ad5bb8dd3389a118ffb
+    private $password = '';
     public $conn;
 
     public function __construct() {
