998395618ef2442eba3be727a8dd397db59691ed
DanielStrath <daniel.morara@strathmore.edu>
2024-11-17 11:10:33 +0300

    myTrips.php and dbConnection.php updated

--- Traveller module/myTrips.php
+++ Traveller module/myTrips.php
@@ -87,49 +87,10 @@ $db = null;
 }
 </style>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Trips</title>
-    <link rel="icon" href="../img/logo2.png" type="image/png">
-
-    <link rel="stylesheet" href="userdash.css">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-
-</head>
-<body>
-
-<div class="nav">
-        <div class="logo">
-            <img src="../img/logo.png" alt="Logo">
-            <h1>CheapThrills</h1>
-        </div>
-
-        <div class="links">
-            <ul>
-                <a href="../NewHomePage.php"><li>Home</li></a>
-                <a href=""><li>Book</li></a>
-                <a href=""><li>About Us</li></a>
-                <a href=""><li>Contact</li></a>
-                <!-- <a href="https://layla.ai/chat?ask=create-a-new-trip"><li>Plan your Trip</li></a> -->
-            </ul>
-        </div>
-
-        <div class="search">
-            <img src="../img/search.png" alt="Search">
-            <input type="search" name="search" id="navSearch" placeholder="Search...">
-        </div>
-
-        <div class="account">
-            <img src="../img/user.png" alt="user">
-            <p>Account</p>
-        </div>
-    </div>
-
-
-    <?php include "sidebar.php" ?>
+<?php
+$pagetitle = "My Trips";
+include 'header.php';   
+?>
 <div class="main-content">
     <h1>Your Booked Travel Packages</h1>
     
