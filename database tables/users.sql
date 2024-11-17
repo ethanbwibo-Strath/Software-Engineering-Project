f123c7711935b7608252dcd62dbc2340aef7f432
Rehana-Kerubo <166706039+Rehana-Kerubo@users.noreply.github.com>
2024-11-17 23:15:13 +0300

    You can now have a profile picture :)

--- database tables/users.sql
+++ database tables/users.sql
@@ -1,12 +1,11 @@
-CREATE DATABASE CheapThrills;
-USE CheapThrills;
-
+DROP TABLE IF EXISTS `Users`;
 CREATE TABLE `Users` (
     `UserID` INT AUTO_INCREMENT PRIMARY KEY,
     `username` VARCHAR(50) NOT NULL UNIQUE,
     `fname` VARCHAR(50) NOT NULL,
     `lname` VARCHAR(50) NOT NULL,
-    `email` VARCHAR(100) NOT NULL UNIQUE,  
+    `email` VARCHAR(100) NOT NULL UNIQUE,
+    `profile_picture` VARCHAR(255), 
     `phone` VARCHAR(15) NOT NULL,          
     `password` VARCHAR(255) NOT NULL,
     `account_type` VARCHAR(50) NOT NULL
